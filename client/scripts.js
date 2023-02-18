a8dc0f007f3697ff9f19f10d9704037abb13d897
Jake Ye <jayyeucla@gmail.com>
2023-02-17 22:45:44 -0800

    update front end

--- client/scripts.js
+++ client/scripts.js
@@ -13,7 +13,7 @@ function loader(element) {
   loadInterval = setInterval(() => {
     element.textContent += '.';
 
-    if(element.textContent === '...') {
+    if(element.textContent === '....') {
       element.textContent = ''
     }
   }, 300)
@@ -32,3 +32,57 @@ function typeText(element, text) {
   }, 20)
 }
 
+function gererateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16)
+
+  return `id-${timestamp}-${hexadecimalString}`;
+
+}
+
+function chatStripe (isAi, value, uniqueId) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div className = "profile">
+            <img
+              src="${isAi ? bot : user}"
+              alt="${isAi ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //user's chatstripe
+  chatContainer.innerHtml += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  const uniqueId = gererateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e)=>{
+  if(e.keyCode === 13) {
+    handleSubmit(e)
+  }
+})
